703cc1db2f1258fb1519eec6b13326389296a0ce
franklin <ikeogufranklin1@gmail.com>
2021-08-10 08:45:04 -0400

    Uncommented the supporting fee functions

--- contracts/OreoswapV1Router02.sol
+++ contracts/OreoswapV1Router02.sol
@@ -9,7 +9,7 @@ import './libraries/SafeMath.sol';
 import './interfaces/IERC20.sol';
 import './interfaces/IWETH.sol';
 
-contract                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        OreoswapV1Router02 is IOreoswapV1Router02 {
+contract OreoswapV1Router02 is IOreoswapV1Router02 {
     using SafeMath for uint;
 
     address public immutable override factory;
@@ -318,86 +318,85 @@ contract                                                                        
 
     // **** SWAP (supporting fee-on-transfer tokens) ****
     // requires the initial amount to have already been sent to the first pair
-    // function _swapSupportingFeeOnTransferTokens(address[] memory path, address _to) internal virtual {
-    //     for (uint i; i < path.length - 1; i++) {
-    //         (address input, address output) = (path[i], path[i + 1]);
-    //         (address token0,) = OreoswapV1Library.sortTokens(input, output);
-    //         IUniswapV2Pair pair = IUniswapV2Pair(OreoswapV1Library.pairFor(factory, input, output));
-    //         uint amountInput;
-    //         uint amountOutput;
-    //         { // scope to avoid stack too deep errors
-    //         (uint reserve0, uint reserve1,) = pair.getReserves();
-    //         (uint reserveInput, uint reserveOutput) = input == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
-    //         amountInput = IERC20(input).balanceOf(address(pair)).sub(reserveInput);
-    //         amountOutput = OreoswapV1Library.getAmountOut(amountInput, reserveInput, reserveOutput);
-    //         }
-    //         (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOutput) : (amountOutput, uint(0));
-    //         address to = i < path.length - 2 ? OreoswapV1Library.pairFor(factory, output, path[i + 2]) : _to;
-    //         pair.swap(amount0Out, amount1Out, to, new bytes(0));
-    //     }
-    // }
-    // function swapExactTokensForTokensSupportingFeeOnTransferTokens(
-    //     uint amountIn,
-    //     uint amountOutMin,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // ) external virtual override ensure(deadline) {
-    //     TransferHelper.safeTransferFrom(
-    //         path[0], msg.sender, OreoswapV1Library.pairFor(factory, path[0], path[1]), amountIn
-    //     );
-    //     uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
-    //     _swapSupportingFeeOnTransferTokens(path, to);
-    //     require(
-    //         IERC20(path[path.length - 1]).balanceOf(to).sub(balanceBefore) >= amountOutMin,
-    //         'OreoswapV1Router: INSUFFICIENT_OUTPUT_AMOUNT'
-    //     );
-    // }
-    // function swapExactETHForTokensSupportingFeeOnTransferTokens(
-    //     uint amountOutMin,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // )
-    //     external
-    //     virtual
-    //     override
-    //     payable
-    //     ensure(deadline)
-    // {
-    //     require(path[0] == WETH, 'OreoswapV1Router: INVALID_PATH');
-    //     uint amountIn = msg.value;
-    //     IWETH(WETH).deposit{value: amountIn}();
-    //     assert(IWETH(WETH).transfer(OreoswapV1Library.pairFor(factory, path[0], path[1]), amountIn));
-    //     uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
-    //     _swapSupportingFeeOnTransferTokens(path, to);
-    //     require(
-    //         IERC20(path[path.length - 1]).balanceOf(to).sub(balanceBefore) >= amountOutMin,
-    //         'OreoswapV1Router: INSUFFICIENT_OUTPUT_AMOUNT'
-    //     );
-    // }
-    // function swapExactTokensForETHSupportingFeeOnTransferTokens(
-    //     uint amountIn,
-    //     uint amountOutMin,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // )
-    //     external
-    //     virtual
-    //     override
-    //     ensure(deadline)
-    // {
-    //     require(path[path.length - 1] == WETH, 'OreoswapV1Router: INVALID_PATH');
-    //     TransferHelper.safeTransferFrom(
-    //         path[0], msg.sender, OreoswapV1Library.pairFor(factory, path[0], path[1]), amountIn
-    //     );
-    //     _swapSupportingFeeOnTransferTokens(path, address(this));
-    //     uint amountOut = IERC20(WETH).balanceOf(address(this));
-    //     require(amountOut >= amountOutMin, 'OreoswapV1Router: INSUFFICIENT_OUTPUT_AMOUNT');
-    //     IWETH(WETH).withdraw(amountOut);
-    //     TransferHelper.safeTransferETH(to, amountOut);
-    // }
+    function _swapSupportingFeeOnTransferTokens(address[] memory path, address _to) internal virtual {
+         for (uint i; i < path.length - 1; i++) {
+             (address input, address output) = (path[i], path[i + 1]);
+             (address token0,) = OreoswapV1Library.sortTokens(input, output);
+             IUniswapV2Pair pair = IUniswapV2Pair(OreoswapV1Library.pairFor(factory, input, output));
+             uint amountInput;
+             uint amountOutput;
+             { // scope to avoid stack too deep errors
+             (uint reserve0, uint reserve1,) = pair.getReserves();
+             (uint reserveInput, uint reserveOutput) = input == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+             amountInput = IERC20(input).balanceOf(address(pair)).sub(reserveInput);
+             amountOutput = OreoswapV1Library.getAmountOut(amountInput, reserveInput, reserveOutput);
+             }
+             (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOutput) : (amountOutput, uint(0));
+             address to = i < path.length - 2 ? OreoswapV1Library.pairFor(factory, output, path[i + 2]) : _to;
+             pair.swap(amount0Out, amount1Out, to, new bytes(0));
+         }
+     }
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+         uint amountIn,
+         uint amountOutMin,
+         address[] calldata path,
+         address to,
+         uint deadline
+     ) external virtual ensure(deadline) {
+         TransferHelper.safeTransferFrom(
+             path[0], msg.sender, OreoswapV1Library.pairFor(factory, path[0], path[1]), amountIn
+         );
+         uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
+         _swapSupportingFeeOnTransferTokens(path, to);
+         require(
+             IERC20(path[path.length - 1]).balanceOf(to).sub(balanceBefore) >= amountOutMin,
+            'OreoswapV1Router: INSUFFICIENT_OUTPUT_AMOUNT'
+         );
+     }
+     function swapExactETHForTokensSupportingFeeOnTransferTokens(
+         uint amountOutMin,
+         address[] calldata path,
+         address to,
+         uint deadline
+     )
+         external
+         virtual
+         payable
+         ensure(deadline)
+     {
+       require(path[0] == WETH, 'OreoswapV1Router: INVALID_PATH');
+         uint amountIn = msg.value;
+         IWETH(WETH).deposit{value: amountIn}();
+         assert(IWETH(WETH).transfer(OreoswapV1Library.pairFor(factory, path[0], path[1]), amountIn));
+         uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
+         _swapSupportingFeeOnTransferTokens(path, to);
+         require(
+             IERC20(path[path.length - 1]).balanceOf(to).sub(balanceBefore) >= amountOutMin,
+             'OreoswapV1Router: INSUFFICIENT_OUTPUT_AMOUNT'
+         );
+    }
+     function swapExactTokensForETHSupportingFeeOnTransferTokens(
+         uint amountIn,
+         uint amountOutMin,
+       address[] calldata path,
+         address to,
+         uint deadline
+    )
+         external
+         virtual
+         
+         ensure(deadline)
+     {
+         require(path[path.length - 1] == WETH, 'OreoswapV1Router: INVALID_PATH');
+         TransferHelper.safeTransferFrom(
+             path[0], msg.sender, OreoswapV1Library.pairFor(factory, path[0], path[1]), amountIn
+         );
+         _swapSupportingFeeOnTransferTokens(path, address(this));
+         uint amountOut = IERC20(WETH).balanceOf(address(this));
+         require(amountOut >= amountOutMin, 'OreoswapV1Router: INSUFFICIENT_OUTPUT_AMOUNT');
+         IWETH(WETH).withdraw(amountOut);
+         TransferHelper.safeTransferETH(to, amountOut);
+     }
 
     // **** LIBRARY FUNCTIONS ****
     function quote(uint amountA, uint reserveA, uint reserveB) public pure virtual override returns (uint amountB) {
